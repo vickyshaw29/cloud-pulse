f8df06bd907aa1b48c90b7d4183340944aa8eaca
Vicky Kumar Prasad <prasadvicky231@gmail.com>
2024-02-03 15:00:00 +0530

    some cleanup

--- firebase.ts
+++ firebase.ts
@@ -1,21 +1,21 @@
-import { getApp, getApps, initializeApp } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore'
-import { getStorage } from 'firebase/storage'
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyB-Xsi6lzr2sBC2KEjwfA3ou1ZDT984iS0",
-  authDomain: "cloud-pulse-1f2b1.firebaseapp.com",
-  projectId: "cloud-pulse-1f2b1",
-  storageBucket: "cloud-pulse-1f2b1.appspot.com",
-  messagingSenderId: "1007503485230",
-  appId: "1:1007503485230:web:497997c8758538e67db2bf"
-};
 
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY as string,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN as string,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID as string,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET as string,
+  messagingSenderId: process.env
+    .NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID as string,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID as string,
+};
 
 const app = getApps()?.length ? getApp() : initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage(app);
 
-export { db, storage }
-
+export { db, storage };
